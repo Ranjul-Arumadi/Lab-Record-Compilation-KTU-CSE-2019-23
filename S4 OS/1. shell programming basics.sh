496c2a2bb8011d79f18841863fda6f7af7a53bee
Ranjul_Arumadi <54582161+Ranjul-Arumadi@users.noreply.github.com>
2021-08-11 20:28:05 +0530

    Create 1. shell programming basics.sh

---
+++ S4 OS/1. shell programming basics.sh
@@ -0,0 +1,21 @@
+<<comment
+Baics of shell programming in Linux
+
+1. single-line comments
+    
+    #comment here
+    
+2. multi line comments 
+    
+    <<samename
+        comments here
+    samename
+
+3. assign value to variable
+
+    varname = value
+    
+4. 
+
+comment
+
