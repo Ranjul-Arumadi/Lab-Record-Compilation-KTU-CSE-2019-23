f67ff75b64bad8007a408ec59097292d2aa99fb8
Ranjul_Arumadi <54582161+Ranjul-Arumadi@users.noreply.github.com>
2021-09-28 08:16:16 +0530

    Create BestFit.c

---
+++ S4 OS/BestFit.c
@@ -0,0 +1,71 @@
+/*
+Best Fit allocation strategy - The best fit deals with allocating the smallest free partition which meets the requirement of the requesting process. This algorithm first searches the entire list of free partitions and considers the smallest hole that is adequate. It then tries to find a hole which is close to actual process size needed.
+*/
+#include<stdio.h>
+int main(){
+	int i,j,memory_count, process_count;
+	
+	printf("\nEnter number of partitions: ");
+	scanf("%d",&memory_count);
+	
+	printf("\nEnter number of processes: ");
+	scanf("%d",&process_count);
+	
+	int memory[memory_count],process[process_count],answer[memory_count];
+	
+	printf("\nEnter the partitions: ");
+	for(i=0;i<memory_count;i++){
+		scanf("%d",&memory[i]);
+	}
+	
+	printf("\nEnter the processes sizes: ");
+	for(i=0;i<process_count;i++){
+		scanf("%d",&process[i]);
+	}
+	
+	//setting the answer array to -1
+	for(i=0;i<memory_count;i++){
+		answer[i]=-1;
+	}
+	
+	int flag=0,temp,index;
+	for(i=0;i<process_count;i++){
+        for(j=0;j<memory_count;j++){
+			if(memory[j]<process[i] || answer[j]!=-1){
+				continue;
+			}
+			else if(memory[j]>process[i] && flag==0){
+				temp=memory[j];
+				index=j;
+				flag=1;
+				continue;
+			}
+			else if(flag==1 && memory[j]<temp && memory[j]>=process[i]){
+				temp = memory[j];
+				index=j;
+			}
+        }
+		if(flag==1){
+			flag=0;
+			answer[index] = i;
+			//upon getting a final answer decrement the memory
+			memory[index]-= process[i];
+		}
+   }
+  
+	//displaying ans.
+	printf("\n\nOutput:\n\n");
+	for(i=0;i<memory_count;i++){
+		if(answer[i]==-1){
+			printf("  X  ");
+		}
+		else{
+			printf("  %d  ",answer[i]);
+		}
+	}
+	printf("\n");
+	for(i=0;i<memory_count;i++){
+		printf(" %d ", memory[i]);
+	}
+	return 0;
+}
