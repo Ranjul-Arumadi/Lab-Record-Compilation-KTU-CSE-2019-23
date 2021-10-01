c25caef6066839b18c101ee717f23d160e115392
Ranjul_Arumadi <54582161+Ranjul-Arumadi@users.noreply.github.com>
2021-10-01 22:50:49 +0530

    Update example.c

--- S4 OS/Using LINUX System Calls/exec()/example.c
+++ S4 OS/Using LINUX System Calls/exec()/example.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-int main(int arga, char *argb[])
+int main(int argc, char *argv[])
 {
     printf("PID of example.c = %d\n", getpid());
     char *args[] = {"Hello", "C", "Programming", NULL};
-    
-    //execv() excutes the other file which has been specified
     execv("./hello", args);
     printf("Back to example.c");
     return 0;
