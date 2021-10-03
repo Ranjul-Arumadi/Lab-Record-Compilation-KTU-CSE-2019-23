e7879d080d44d3897b2644c709a2b86b9f4253d5
Ranjul_Arumadi <54582161+Ranjul-Arumadi@users.noreply.github.com>
2021-10-03 08:25:07 +0530

    Update and rename IPCsharedMemory.c to IPCsharedMemory1.c

--- S4 OS/Using LINUX System Calls/IPCsharedMemory1.c
+++ S4 OS/Using LINUX System Calls/IPCsharedMemory1.c
@@ -23,7 +23,10 @@ The first parameter here is the identifier which shmget() function returns on su
 int main()
 {
 	int i;
+	
+	//imp
 	void *shared_memory;
+	
 	char buff[100];
 	int shmid;
 	
@@ -34,12 +37,14 @@ int main()
 	
 	shared_memory=shmat(shmid,NULL,0); //process attached to shared memory segment
 	
-	
+	//IMP- %p
 	printf("Process attached at %p\n",shared_memory); //this prints the address where the segment is attached with this process
 	printf("Enter some data to write to shared memory\n");
 	
 	
 	read(0,buff,100); //get some input from user
+	
+	//copy user input to shared memory
 	strcpy(shared_memory,buff); //data written to shared memory
 	printf("You wrote : %s\n",(char *)shared_memory);
 }
