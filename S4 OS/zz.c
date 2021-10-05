a73a4b740ab71c1f008a800dc2fd3e7cb9bffd07
Ranjul_Arumadi <54582161+Ranjul-Arumadi@users.noreply.github.com>
2021-10-05 07:42:15 +0530

    Update zz.c

--- S4 OS/zz.c
+++ S4 OS/zz.c
@@ -141,3 +141,24 @@ read(0,buff,100);
 strcpy(shared_memory,buff);
 printf("You wrote : %s\n",(char *)shared_memory);
 }
+
+
+//shm2
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/shm.h>
+#include<string.h>
+int main()
+{
+int i;
+void *shared_memory;
+char buff[100];
+int shmid;
+shmid=shmget((key_t)2345, 1024, 0666);
+printf("Key of shared memory is %d\n",shmid);
+shared_memory=shmat(shmid,NULL,0);
+printf("Process attached at %p\n",shared_memory);
+printf("Data read from shared memory is : %s\n",(char *)shared_memory);
+}
